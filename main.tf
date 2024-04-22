c71df693f5886bc60df05ff61f5fcc365afc9ad7
Reshma Stani <reshma@corra.com>
2024-04-22 17:02:28 +0530

    terraform 00~Unsupported argument machine_type on line 12,13,14,15,16

--- main.tf
+++ main.tf
@@ -7,13 +7,19 @@ resource "google_container_cluster" "my_cluster" {
   name     = "my-cluster"
   location = "us-central1"
 
-  node_pool {
-    name       = "default-pool"
+  node_config {
     machine_type = "e2-medium"
     disk_size_gb = 10
-    disk_type = "pd-standard"
-    min_count = 1
-    max_count = 3
-    node_count = 1
+    disk_type    = "pd-standard"
+  }
+
+  node_pool {
+    name       = "default-pool"
+    initial_node_count = 1
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 3
+    }
   }
 }
+
